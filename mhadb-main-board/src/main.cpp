0b3fe6bc576088165dfbc70d609bab8003463ea1
Rémi Ait-Younes <remi@ait-younes.fr>
2026-01-20 19:24:22 +0100

    main: add can handler test

--- mhadb-main-board/src/main.cpp
+++ mhadb-main-board/src/main.cpp
@@ -2,9 +2,26 @@
 #include "can_messages.h"
 
 
-class LineCanHandler
+class MainCanHandler
 {
   public:
+  static void handle_struct(t_bldc_current_pos data) {
+    // Serial.printf("Line pos: %d\n", data.shaft_angle);
+    // Serial.printf("yay: %f", data.shaft_angle);
+    Serial.print("oof: ");
+    Serial.println(data.shaft_angle);
+    // Serial.println(sizeof(t_bldc_current_pos));
+  }
+  static void handle_struct(t_bldc_current_speed data) {
+    Serial.print("speed: ");
+    Serial.println(data.speed);
+  }
+
+  static void handle_struct(t_bldc_alignment_results data) {
+    Serial.print("zero angle: ");
+    Serial.println(data.zero_electric_angle);
+    Serial.printf("direction: %d\n", data.sensor_direction);
+  }
 
   template<typename T>
   static void handle_struct(T data) {};
@@ -22,8 +39,10 @@ void setup() {
 }
 
 void loop() {
-  t_line_sensor_raw_data a {.id=10, .value=126};
-  can.send_struct(a);
-  delay(1000);
+  // t_line_sensor_raw_data a {.id=10, .value=126};
+  // can.send_struct(a);
+  can.send_rtr(CAN_ID::LINE_RAW_SENSOR_DATA);
+  can.handle_can();
+  delay(50);
   Serial.println("alive");
 }
