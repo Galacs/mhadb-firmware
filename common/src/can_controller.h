f08ccd31c9dc0c78f1f5f4fc7d659da9cb20565c
Rémi Ait-Younes <remi@ait-younes.fr>
2026-01-20 18:58:05 +0100

    can: fix can controller esp32 support

--- common/src/can_controller.h
+++ common/src/can_controller.h
@@ -43,8 +43,8 @@ struct t_can_frame {
 template<typename Child>
 class CanController {
 public:
-  CanController(): m_stm32CAN(CAN1, DEF) {};
 #ifdef ARDUINO_ARCH_STM32
+  CanController(): m_stm32CAN(CAN1, DEF) {};
   void init() {
     m_stm32CAN.begin();
     m_stm32CAN.setBaudRate(100000);
@@ -77,7 +77,55 @@ public:
   }
 #endif
 #ifdef ARDUINO_ARCH_ESP32
-  void init(gpio_num_t rx, gpio_num_t tx);
+  CanController() {};
+  void init(gpio_num_t rx, gpio_num_t tx) {
+    // twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(tx, rx, TWAI_MODE_NORMAL);
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(tx, rx, TWAI_MODE_NO_ACK);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    // Install TWAI driver
+    if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
+        Serial.println("Driver installed");
+    } else {
+        Serial.println("Failed to install driver");
+        return;
+    }
+
+    // Start TWAI driver
+    if (twai_start() == ESP_OK) {
+        Serial.println("Driver started");
+    } else {
+        Serial.println("Failed to start driver");
+        return;
+    }
+  };
+
+  void send_can(uint32_t id, uint8_t *data, uint8_t data_len) {
+      twai_message_t message;
+      message.identifier = CAN_ID::LINE_RAW_SENSOR_DATA;
+      message.data_length_code = data_len;
+      Serial.printf("data len: %d\n", data_len);
+      memcpy(&message.data, data, data_len);
+      if (twai_transmit(&message, pdMS_TO_TICKS(1000)) == ESP_OK) {
+          printf("Message queued for transmission\n");
+      } else {
+          printf("Failed to queue message for transmission\n");
+      }
+  };
+
+  bool receive_can(t_can_frame* frame) {
+      //uint32_t alerts_triggered;
+      //twai_read_alerts(&alerts_triggered, 0);
+      twai_message_t message;
+      if (twai_receive(&message, 0) == ESP_ERR_TIMEOUT) {
+          Serial.println("no can frames");
+          return false;
+      }
+      memcpy(frame->buf, &message.data, message.data_length_code);
+      return true;
+  };
+
 #endif
   
   void send_rtr(uint32_t msg_id) {
