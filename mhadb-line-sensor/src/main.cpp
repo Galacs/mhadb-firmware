b50a643807bf78c9527e7e82a74d3e33d31846c1
Rémi Ait-Younes <remi@ait-younes.fr>
2026-02-04 21:24:37 +0100

    line: remove old unused code

--- mhadb-line-sensor/src/main.cpp
+++ mhadb-line-sensor/src/main.cpp
@@ -185,215 +185,107 @@ unsigned long t_start = 0;
 unsigned long nine_start = 0;
 
 int16_t get_line_position(uint16_t* values) {
-  // fonction 1 rémi
-  #if 0
-  // TODO: Moyenne réduite ou cubique ou mieux
-  const uint8_t weights[] = {-44, -34, -24, -14, -4, 4, 14, 24, 34, 44};
-  // Devboard
-  // const uint8_t weights[] = {30, 30, 30, 30, 30};
-  int32_t total_moy = 0;
-  int total = 0;
-  for (uint8_t i = 0; i < 10; i++) {
-    // Serial.println(weights[i % 4] * values[i] * ((i < 4)? 1: -1));
-    // TODO: les poids ne sont pas symmétriques
-    total_moy += weights[i % 4] * values[i] * ((i < 4)? 1: -1);
-    total += values[i];
-  }
-  // Serial.printf("Line pos: %d \t", total_moy);
-  // Devboard
-  // return total_moy/(total);
-  return total_moy;
-  //#endif
-  // fonction 1 victor
-  if 0
-  double coef = 0.95;
-  int max=0, k=0, sum=0; 
-  int tab[10]={0}; 
 
-  for (int i=0; i<10; i++) {
-    if (values[i] > max) max=values[i];
-  }
-  for (int j=0;j<10;j++) {
-    if (values[j] > max*coef){
-      tab[k] = j;
-      //sum += values[j];
-      sum += j;
-      k++;
-    }
-  }
-  return (sum/k);
-  #endif
-  // fonction 2 victor
-  #if 0
-  int nb_val=10;
-  int size_group = 2;
+  // const int8_t weights[] = {-44, -34, -24, -14, -4, 4, 14, 24, 34, 44};
+  // int total = 0, moy_pon, sum=0;
+  // for (int i = 0; i < 10; i++) {
+  //   // values[i] = 100 - values[i];
+  //   // if (values[i] < 900)
+  //   //   values[i] = 0;
+  //   total+=weights[i]*(mapped_values[i]/10);
+  //   sum+=(mapped_values[i]/10);
+  // }
+  // moy_pon = (total*100)/sum;
   
+  // // Set last side
+  // if (mapped_values[0] > 30) {
+  //   last_side = -1;
+  // } else if (mapped_values[9] > 30) {
+  //   last_side = 1;
+  // }
 
-  int tab[nb_val]={0};
-  int max=0, sum=0, calc, moy_val, moy_pos, count=0, max_ch=0, pos=0;
+  // int a = 0;
+  // for (int i = 0; i < 10; i++) {
+  //   a += mapped_values[i]/10;
+  // }
+  // // T junction detection
+  // if (a > 450) {
+  //   if (abs(moy_pon) < 2500 ) {
+  //     line_state = line_pos_state_t::FULL;
+  //     is_full = true;
+  //     last_full = millis();
+  //   }
+  //   // Left/right 90
+  // } else if (values[0] + values[1] + values[2] + values[3] + values[4] > 5*50) {
+  //   // line_state = line_pos_state_t::TURN;
+  //   // nine_start = millis();
+  // } else if (values[5] + values[6] + values[7] + values[8] + values[9] > 5*50) {
+  //   // line_state = line_pos_state_t::TURN;
+  //   // nine_start = millis();
+  // }
+  // if (line_state == line_pos_state_t::T && millis() < t_start + 800) {
+  //   return 4400;
+  // } else {
+  //   t_start = 0;
+  // }
+  // if (line_state == line_pos_state_t::TURN && millis() < nine_start + 300) {
+  //   return last_side*4400;
+  // } else {
+  //   nine_start = 0;
+  // }
 
-  if (nb_val>10) {
-    for(int i=0; i<nb_val; i++){
-      if (i%2==0){
-        calc=values[i/2];
-        tab[i]=calc;
-        sum+=calc;
-        if (calc > max) max=calc;
-      }
-      else {
-        calc=int(values[((i-1)/2)]+values[((i+1)/2)]);
-        tab[i]=calc;
-        sum+=calc;
-        if (calc > max) max=calc;
-      }
-    }
-  }
-  if (nb_val==10){
-    for (int i=0;i<10;i++) {
-      sum+=values[i];
-      tab[i] = values[i];
-    }
-  }
-
-  moy_val=int(sum/nb_val);
-  for(int j=0; j<nb_val; j++){
-    if (tab[j]<=moy_val) tab[j]=0;
-  }
-
-  if (size_group == 2) {
-    for (int k=2;k<nb_val;k++){
-      if((tab[k])&& ((tab[k+1])||(tab[k-1])) ) continue;
-      else tab[k]=0;
-    }
-  }
-  if (size_group == 3) {
-    for (int k=2;k<nb_val;k++){
-      if( (tab[k])&& ((tab[k+1]) && (tab[k+2])  ||  (tab[k-1] && tab[k-2]) || (tab[k-1] && tab[k+1])) ) continue;
-      else tab[k]=0;
-    }
-  }
-
-  count = 0;
-  max_ch = 0;
-  for(int i=1;i<nb_val;i++) {
-    if (tab[i-1] && tab[i]) {
-      count++;
-      if ((tab[i-2]==0)&& i!=1) pos=i-1;
-    }
-    else {
-      if (count>max) max_ch=count;
-      count=0;
-    }
-  } // potentiellement pos=0 et max_ch=0
-
-  sum=0;
-  for(int i=pos;i<pos+count;i++){
-    sum+=i;
-  }
-  return float(sum/count);
-  #endif
-
-
-  const int8_t weights[] = {-44, -34, -24, -14, -4, 4, 14, 24, 34, 44};
-  int total = 0, moy_pon, sum=0;
-  for (int i = 0; i < 10; i++) {
-    // values[i] = 100 - values[i];
-    // if (values[i] < 900)
-    //   values[i] = 0;
-    total+=weights[i]*(mapped_values[i]/10);
-    sum+=(mapped_values[i]/10);
-  }
-  moy_pon = (total*100)/sum;
-  
-  // Set last side
-  if (mapped_values[0] > 30) {
-    last_side = -1;
-  } else if (mapped_values[9] > 30) {
-    last_side = 1;
-  }
-
-  int a = 0;
-  for (int i = 0; i < 10; i++) {
-    a += mapped_values[i]/10;
-  }
-  // T junction detection
-  if (a > 450) {
-    if (abs(moy_pon) < 2500 ) {
-      line_state = line_pos_state_t::FULL;
-      is_full = true;
-      last_full = millis();
-    }
-    // Left/right 90
-  } else if (values[0] + values[1] + values[2] + values[3] + values[4] > 5*50) {
-    // line_state = line_pos_state_t::TURN;
-    // nine_start = millis();
-  } else if (values[5] + values[6] + values[7] + values[8] + values[9] > 5*50) {
-    // line_state = line_pos_state_t::TURN;
-    // nine_start = millis();
-  }
-  if (line_state == line_pos_state_t::T && millis() < t_start + 800) {
-    return 4400;
-  } else {
-    t_start = 0;
-  }
-  if (line_state == line_pos_state_t::TURN && millis() < nine_start + 300) {
-    return last_side*4400;
-  } else {
-    nine_start = 0;
-  }
-
-  if (a < 5) {
-    if (line_state == line_pos_state_t::LOST && millis() > 5000 + no_line_time) {
-      no_line_time = 0;
-      line_state = line_pos_state_t::NO_LINE;
-      return 0;
-    }
-    if (is_full && (millis() > last_full + 3000)) {
-      line_state = line_pos_state_t::T;
-      is_full = false;
-      t_start = millis();
-      return 4400;
-    }
-    if (!lost_time) {
-      lost_time = millis();
-      line_state = line_pos_state_t::LOSTING;
-    }
-    if (line_state == line_pos_state_t::LOSTING) {
-      if ((lost_time + 2000) < millis()) {
-        line_state = line_pos_state_t::LOST;
-        pos_before_lost = 0;
-        lost_time = 0;
-        no_line_time = millis();
-        return 0;
-      }
-      // if (pos_before_lost > 0) {
-      //   return 4400; {}
-      // } else {
-      //   return -4400;
-      // } 
-      if (last_side == 1) {
-        return 4400;
-      } else if (last_side == -1) {
-        return -4400;
-      } 
-    }
-    line_state = line_pos_state_t::NO_LINE;
-  } else if (a > 300) {
-    // Left/Right turn
-    if (moy_pon > 0) {
-      // moy_pon += 2000;
-    } else {
-      // moy_pon -= 2000;
-    }
-  } else {
-    line_state = line_pos_state_t::DETECTED;
-    lost_time = 0;
-    last_full = 0;
-  }
+  // if (a < 5) {
+  //   if (line_state == line_pos_state_t::LOST && millis() > 5000 + no_line_time) {
+  //     no_line_time = 0;
+  //     line_state = line_pos_state_t::NO_LINE;
+  //     return 0;
+  //   }
+  //   if (is_full && (millis() > last_full + 3000)) {
+  //     line_state = line_pos_state_t::T;
+  //     is_full = false;
+  //     t_start = millis();
+  //     return 4400;
+  //   }
+  //   if (!lost_time) {
+  //     lost_time = millis();
+  //     line_state = line_pos_state_t::LOSTING;
+  //   }
+  //   if (line_state == line_pos_state_t::LOSTING) {
+  //     if ((lost_time + 2000) < millis()) {
+  //       line_state = line_pos_state_t::LOST;
+  //       pos_before_lost = 0;
+  //       lost_time = 0;
+  //       no_line_time = millis();
+  //       return 0;
+  //     }
+  //     // if (pos_before_lost > 0) {
+  //     //   return 4400; {}
+  //     // } else {
+  //     //   return -4400;
+  //     // } 
+  //     if (last_side == 1) {
+  //       return 4400;
+  //     } else if (last_side == -1) {
+  //       return -4400;
+  //     } 
+  //   }
+  //   line_state = line_pos_state_t::NO_LINE;
+  // } else if (a > 300) {
+  //   // Left/Right turn
+  //   if (moy_pon > 0) {
+  //     // moy_pon += 2000;
+  //   } else {
+  //     // moy_pon -= 2000;
+  //   }
+  // } else {
+  //   line_state = line_pos_state_t::DETECTED;
+  //   lost_time = 0;
+  //   last_full = 0;
+  // }
 
 
-  pos_before_lost = moy_pon;
-  return moy_pon; 
+  // pos_before_lost = moy_pon;
+  // return moy_pon; 
 }
 
 void update_can() {
